a07a27433ea71542c839db1129944521cbe25dbc
Robin van der Knaap <robin@skaele.nl>
2015-05-06 20:04:14 +0200

    Added UI router

---
+++ src/routes.js
@@ -0,0 +1,20 @@
+(function(){
+
+	var app = angular.module('app');
+
+	app.config(function($stateProvider, $urlRouterProvider){
+
+		// For any unmatched url, redirect to home
+  		$urlRouterProvider.otherwise("/");
+
+  		$stateProvider
+		    .state('home', {
+		      url: "/",
+		      templateUrl: "modules/home/home.html"
+		    })
+		    .state('about', {
+		      url: "/about",
+		      templateUrl: "modules/about/about.html"
+		    })
+	});
+}());
